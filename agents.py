03fd00875f04a89451301c9bf67f81a5a2d02237
Tianpeng Zhang <tzhang@g.harvard.edu>
2022-11-25 13:34:58 -0500

    Optimize code performance by replacing np.mean() with incremental r_sum tracking. Also separate the Q-learning agent out from the graph_bandit environment.

--- agents.py
+++ agents.py
@@ -3,14 +3,16 @@ import numpy as np
 from known_rewards_helper_functions import offline_SP_planning,EVI_known_transition_planning
 
 
-def get_ucb(gb,nodes=None):
+def get_ucb(env,nodes=None):
     
     if nodes is None:
-        nodes = gb.nodes
-    ave_reward = [np.mean(gb.nodes[i]['r_hist']) for i in nodes] 
-    nm = [gb.nodes[i]['n_visits'] for i in nodes]
+        nodes = env.nodes
+
+    nm = np.array([env.nodes[i]['n_visits'] for i in nodes])
     
-    tm = len(gb.visitedStates)
+    ave_reward = np.array([env.nodes[i]['r_sum']for i in nodes])/nm
+
+    tm = len(env.visitedStates)
     ucb = ave_reward + np.sqrt(2*np.log(tm)/nm)
     
     return ucb
@@ -43,7 +45,10 @@ def get_ts_parameters(env,neighbors,
                     var_0 = 1.0,
                     mu_0 = 0.0,
                     var = 1.0):
-    xsum = np.array([np.sum(env.nodes[i]['r_hist']) for i in neighbors])
+
+      
+    xsum = np.array([env.nodes[i]['r_sum']for i in neighbors])
+
     n = np.array([env.nodes[i]['n_visits'] for i in neighbors])
 
     var_1 = 1/(var_0 + n/var) 
@@ -78,8 +83,9 @@ def UCRL2_ucb(env,nodes=None,delta = 0.01):
     if nodes is None:
         nodes = env.nodes
         
-    ave_reward = np.array([np.mean(env.nodes[i]['r_hist']) for i in nodes] )
     nm = np.array([env.nodes[i]['n_visits'] for i in nodes])
+    ave_reward = np.array([env.nodes[i]['r_sum']for i in nodes])/nm
+
     
     tm = len(env.visitedStates)
     
@@ -132,4 +138,75 @@ def UCRL2_agent(env,delta = 0.01):
 #         visits_this_episode[env.state]+=1
 #         next_s = policy[env.state]
 #         env.step(next_s)
-#             
+         
+class QL_agent:
+    
+    def __init__(self,alpha=0.1, gamma=0.9):
+                 
+        """
+        param alpha: Q-learning parameter (if applicable)
+        param gamma: RL discount factor (if applicable)
+        param epsilon: exploration parameter (if applicable)
+        """
+    
+        # The state-value list.
+        self.q_table = None
+        
+        # Q-Learning Parameters
+        self.alpha = alpha
+        self.gamma = gamma
+        
+    def __call__(self,env):
+           
+        num_nodes = env.G.number_of_nodes()
+        
+        if self.q_table is None:
+             # Initialize Q-table
+            self.q_table = np.zeros((num_nodes,num_nodes))      
+
+            # Eliminate actions that are 'illegal' (agent can only go via edges)
+            for i in range(num_nodes):
+                for j in range(num_nodes):
+                    if (i,j) not in env.G.edges:
+                        self.q_table[i,j] = -np.inf
+           
+        h = len(env.visitedStates)
+
+        state = env.state
+
+        # Caculate the exploration strength epsilon
+
+        epsilon = 0.7*(3*num_nodes + 1) / (3*num_nodes + h)
+        
+        neighbors = list(env.G[state])
+        if np.random.uniform(0, 1) < epsilon:
+            # 'Greedy efficient' exploration: Returns least explored available action.
+            N_explorations = [env.nodes[nb]['n_visits'] for nb in neighbors]
+            action = neighbors[np.argmin(N_explorations)]
+            assert((state,action) in env.G.edges)
+        else:
+            action = np.argmax(self.q_table[state]) # Exploit learned values
+            assert((state,action) in env.G.edges)
+
+        # Next state, reward, and store reward
+        if action is not None:
+            
+            
+            next_state, QL_reward, done = env.step(action)
+            
+            next_state = action
+            old_value = self.q_table[state, action]
+
+            assert(np.isfinite(old_value))
+            next_max = np.max(self.q_table[next_state])
+
+            for node in env.G[action]:
+                
+                old_value = self.q_table[node, action]
+                mu_hat = env.nodes[node]['r_sum']/env.nodes[node]['n_visits']
+               
+                
+                # new_value = (1 - self.alpha) * old_value + self.alpha * (QL_reward + self.gamma * next_max)
+                new_value = mu_hat + self.gamma * next_max
+                
+                self.q_table[node, action] = new_value
--- core.py
+++ core.py
@@ -3,60 +3,73 @@ import numpy as np
 from tqdm import trange
 
 import graph_bandit
+import inspect
+from functools import partial
 
 
-
-def visit_all_nodes(gb):
-    n_nodes  = len(gb.G)
+def visit_all_nodes(env):
+    n_nodes  = len(env.G)
     while True:
-        unvisited = [i for i in range(n_nodes) if gb.nodes[i]['n_visits']==0]
+        unvisited = [i for i in range(n_nodes) if env.nodes[i]['n_visits']==0]
         
         if len(unvisited)==0:
             break
 
         dest = unvisited[0]
         
-        next_path = nx.shortest_path(gb.G,gb.state,dest)
+        next_path = nx.shortest_path(env.G,env.state,dest)
         if len(next_path)==1:
-            gb.step(dest)
+            env.step(dest)
         else:
             for s in next_path[1:]:
-                gb.step(s)
+                env.step(s)
 
 
 
-def train_agent(n_samples,T,G,means, init_node,execute_agent):
-    regrets = np.zeros((n_samples,T))
-    for i in trange(n_samples):
+# def train_agent(n_samples,T,G,means, init_node,agent):
+#     regrets = np.zeros((n_samples,T))
+#     for i in trange(n_samples):
 
-        env = graph_bandit.GraphBandit(means[i],  G)
+#         env = graph_bandit.GraphBandit(means[i],  G)
+#         if inspect.isfunction(agent) or type(agent)==partial:
+#             execute_agent = agent
+#         elif inspect.isclass(agent):
+#             execute_agent = agent()
 
-        ## Visit all nodes
-        visit_all_nodes(env)
 
-        H0 = len(env.visitedStates)
 
-        # Start learning
+#         ## Visit all nodes
+#         visit_all_nodes(env)
 
-        env.state = init_node
+#         H0 = len(env.visitedStates)
 
-        while len(env.visitedStates)-H0<T:
-            execute_agent(env)
+#         # Start learning
+
+#         env.state = init_node
+
+#         while len(env.visitedStates)-H0<T:
+#             execute_agent(env)
             
-        # print(env.visitedStates.shape,regrets.shape)
+#         # print(env.visitedStates.shape,regrets.shape)
             
-        # regrets[i,:]= env.expectedRegret()[:T]
+#         # regrets[i,:]= env.expectedRegret()[:T]
         
-        regrets[i,:]= env.expectedRegret()[-T:]
+#         regrets[i,:]= env.expectedRegret()[-T:]
         
         
-    return regrets
+#     return regrets
 
 
 from joblib import Parallel, delayed,cpu_count
-def parallel_train_agent(n_samples,T,G,means, init_node,execute_agent):
+def train_agent(n_samples,T,G,means, init_node,agent,parallelized=False):
     def main_loop(i):
+
         env = graph_bandit.GraphBandit(means[i],  G)
+
+        if inspect.isfunction(agent) or type(agent)==partial:
+            execute_agent = agent
+        elif inspect.isclass(agent):
+            execute_agent = agent()
 
         ## Visit all nodes
         visit_all_nodes(env)
@@ -77,8 +90,12 @@ def parallel_train_agent(n_samples,T,G,means, init_node,execute_agent):
         return env.expectedRegret()[-T:]
     
     
-    
-    regrets = Parallel(n_jobs = cpu_count())(delayed(main_loop)(i) for i in range(n_samples))
-    
+    if parallelized:
+        regrets = Parallel(n_jobs = cpu_count())(delayed(main_loop)(i) for i in range(n_samples))
+    else:
+        regrets = np.zeros((n_samples,T))
+        for i in trange(n_samples):
+            regrets[i,:] = main_loop(i)
+
         
     return np.array(regrets)

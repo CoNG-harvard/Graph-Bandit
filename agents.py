aa265943cb569130f5b352f1df3a4f5e60af83f0
Tianpeng Zhang <tzhang@g.harvard.edu>
2022-11-26 03:03:39 -0500

    Routine save. Finished plotting major figures.

--- agents.py
+++ agents.py
@@ -114,31 +114,7 @@ def UCRL2_agent(env,delta = 0.01):
         next_s = policy[env.state]
         env.step(next_s)
 
-
-# def PSRL_agent(env,var_0 = 1,
-#                     mu_0 = 0.0,
-#                     var = 1):
-
-
-#     # Sample distributions.
-#     mu_1,var_1 = get_ts_parameters(env,env.nodes, var_0,mu_0,var)
-#     sampled_means = np.random.normal(mu_1,np.sqrt(var_1))
-
-#     # Compute the epsilon-optimal policy based on the sampled means
-#     tm = len(env.visitedStates)
-#     epsilon = 1/np.max([1,np.sqrt(tm)])
-#     policy,_ = EVI_known_transition_planning(env.G,sampled_means,epsilon = epsilon)
-
-#     prev_visits = {s:env.nodes[s]['n_visits'] for s in env.G}
-#     visits_this_episode = {s:0 for s in env.G}
-
-#     # Keep executing the policy until the doubling terminal condition specified in Agrawal(2017), which is the same as in Jacksh(2008), is meet.
-#     while visits_this_episode[env.state]< np.max([1,prev_visits[env.state]]):
-#     # for i in range(10):
-#         visits_this_episode[env.state]+=1
-#         next_s = policy[env.state]
-#         env.step(next_s)
-         
+  
 class QL_agent:
     
     def __init__(self,alpha=0.4, gamma=0.9):
@@ -190,7 +166,6 @@ class QL_agent:
 
         # Next state, reward, and store reward
         if action is not None:
-            
             
             next_state, reward, done = env.step(action)
             
@@ -282,4 +257,97 @@ class QL_UCB_Hoeffding:
                 self.q_table[node, action] \
                         = (1-alpha) * old_value +\
                               alpha * (reward + self.gamma * np.max(self.q_table[action,:]) + b)\
-                             
+
+
+# def PSRL_agent(env,var_0 = 1,
+#                     mu_0 = 0.0,
+#                     var = 1):
+
+
+#     # Sample distributions.
+#     mu_1,var_1 = get_ts_parameters(env,env.nodes, var_0,mu_0,var)
+#     sampled_means = np.random.normal(mu_1,np.sqrt(var_1))
+
+#     # Compute the epsilon-optimal policy based on the sampled means
+#     tm = len(env.visitedStates)
+#     epsilon = 1/np.max([1,np.sqrt(tm)])
+#     policy,_ = EVI_known_transition_planning(env.G,sampled_means,epsilon = epsilon)
+
+#     prev_visits = {s:env.nodes[s]['n_visits'] for s in env.G}
+#     visits_this_episode = {s:0 for s in env.G}
+
+#     # Keep executing the policy until the doubling terminal condition specified in Agrawal(2017), which is the same as in Jacksh(2008), is meet.
+#     while visits_this_episode[env.state]< np.max([1,prev_visits[env.state]]):
+#     # for i in range(10):
+#         visits_this_episode[env.state]+=1
+#         next_s = policy[env.state]
+#         env.step(next_s)
+
+# class GEXP3_agent:
+
+#     def __init__(self):
+#         self.w = None
+#         self.pi = None
+    
+#     def __call__(self,env,eta = 0.01,gamma = 0.01):
+#         '''
+#             eta: the weight in the Exponent.
+#             gamma: learning rate.
+#         '''
+#         G = nx.to_directed(env.G)
+            
+#         if self.w is None:
+#             # Inialize the weight matrix w to be uniformly weighted.
+#             # w[s,a] =  the weight for (s,a)
+#             self.w = nx.adjacency_matrix(G)*1.0
+#             self.pi = np.zeros(self.w.shape)
+#             self.q = np.zeros(self.w.shape)
+
+#         ws = np.array(np.sum(self.w,axis=1)).ravel()
+
+#         for (s,a) in G.edges:
+#             self.pi[s,a] = (1-gamma) * self.w[s,a]/ws[s] + gamma / len(G[s])
+
+#         # visits_this_episode[env.state]+=1
+#         action = np.random.choice(G, p = np.array(self.pi[env.state]).ravel())
+#         env.step(action)
+            
+           
+
+#         # Calculate the policy matrix, pi, based on the Exp3 formula.
+#         eigval,eigvec = np.linalg.eig(self.pi)
+
+#         stationary = np.array(eigvec[:,np.argmax(eigval)]).ravel()
+#         # The stationary distribution of a transition probability matrix is given by its the leading eigenvector(the one with eigenvalue 1)/
+
+#         stationary/=np.linalg.norm(stationary,ord=1) # Normalize the vector so that its entries sum to 1.
+
+#         mu_hat = np.array([env.nodes[s]['r_sum']/env.nodes[s]['n_visits'] for s in G])
+#         rho = stationary.dot(mu_hat) # rho: the estimated long-term average reward. Stateless. This is also different from the original paper.
+        
+#         rhat = np.zeros(self.pi.shape)
+#         # for s in G:
+#         #     rhat[s,:] = mu_hat * self.pi[s,:]
+#         for nb in G[action]: # This is a difference from the paper. We update more entries.
+#             rhat[nb,action] = mu_hat[action]/self.pi[nb,action] 
+
+#         self.q,_ = self.solve_value_functions(G,self.pi,rhat,rho)
+#         # Update w with q
+#         for (s,a) in G.edges:
+#             self.w[s,a] = np.max([5000,self.w[s,a]*np.exp(eta*self.q[s,a])])
+
+
+#     def solve_value_functions(self,G,pi,rhat,rho,max_iter = 1, epsilon = 0.001):
+#         # Iteratively solve for the Q and V functions via Bellman, until convergence
+#         M = G.number_of_nodes()
+#         V = np.zeros(M)
+#         Q = np.zeros((M,M))
+#         for _ in range(max_iter):
+#             old_V = np.array(V)
+#             for (s,v) in G.edges:
+#                 Q[s,v] = rhat[s,v] - rho + V[v]
+
+#             V = np.array([pi[s,:].dot(Q[s,:]) for s in G])
+#             if np.max(V-old_V) - np.min(V-old_V)<epsilon:
+#                 break
+#         return Q,V         
--- graph_bandit.py
+++ graph_bandit.py
@@ -6,20 +6,16 @@ class GraphBandit:
     The graph bandit implemented as a gym environment
     """
     
-    def __init__(self, mean, G, belief_update=None, bayesian_params=[0, 1, 1], init_state=0):
+    def __init__(self, mean, G, init_state=0):
         """
         param mean: Vector of means for bandits (one mean for each node in graph G).
         param G: networkx Graph.
-        param belief_update: How to update beliefs (when applicable (in Q-learning algorithms)). None=use sample rewards in QL-algorithms, average=use average, Bayesian=use Bayesian update. 
-        param bayesian_params: [mu0, sigma0^2, sigma^2]; a list of the belief parameters. mu0 is the initial mean estimate, sigma0 is the initial standard deviation in the initial belief, and sigma is the known variance of the reward distributions. All parameters are the same for all nodes.
-        param init_state: the initial state (node) of the agent.
-        param uncertainty: Constant in UCB estimate; e.g. mu_UCB = mu_est + uncertainty/sqrt(t)
+        param init_state: the initial state (node) of the agent.      
         """
         # Initialization parameters
         self.mean = mean.copy()
         self.G = G.copy()
-        self.belief_update = belief_update
-        self.bayesian_params = bayesian_params.copy()
+        
         self.state = init_state
         self.nodes = self.G.nodes
         self.edges = self.G.edges
